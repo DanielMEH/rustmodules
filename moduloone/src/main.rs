38b9fbd0f2bbbb24463aa7fa42506e6701651d72
@ospinaortizjuandaniel351 <=>
2025-12-24 08:34:21 -0500

    modile initial repository

---
+++ moduloone/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let x = 23;
+    println!("Hello, world! v1 ww {x}");
+}
+
+/* === FUNDAMENTOS RUST ==== */
+/* === INTERFAS DE COMANDO EN LINEA(CLAP) ==== */
+/* === WEBASSEMBLY(LEPTOS,DIOXUS,SPIN FRAMEWORK) ==== */
+/* === BACKEND(TAURI,ACTIXWEB,AXUM,SEA-ORM,SQLX) ==== */
+/* === INTELIGENCIA ARTIFICIAL(BURN,RMCP( MODEL CONTENT PROTOCOLO)) ==== */
+/* === OTROS(SISTEMAS OPERATIVOS,SISTEMAS EMBEBIDOS,BLOCKHAIN) ==== */
+/* === INITIALIZE RUST ==  */
+/*========================0 */
